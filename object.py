472700d16e20d05b4bf368b95773e515688bba95
Bartosz Nitka <niteria@gmail.com>
2026-04-05 19:13:10 +0200

    bump build123d to 0.10.0, drop unmaintained build123d_draft

--- object.py
+++ object.py
@@ -2,7 +2,6 @@
 import logging
 
 from build123d import *  # pyright:ignore
-from build123d_draft import *
 
 import build123d
 import util
@@ -10,63 +9,23 @@ logging.basicConfig(level=logging.DEBUG)
 
 from yacv_server import show
 
-top_dia = 22.3
-weight_top_height = 12
-screw_cap_height = 5
-height = weight_top_height + screw_cap_height
-wall = 1.2
-m8_od = 7.8
+L, w, t, b, h, n = 60.0, 18.0, 9.0, 0.9, 90.0, 8.0
 
-inner = extrude(Circle(radius=top_dia / 2), height)
-outer = extrude(Circle(radius=top_dia / 2 + wall), height + wall)
-hole = extrude(Circle(radius=m8_od / 2), height + wall)
+l1 = Line((0, 0), (0, w / 2))
+l2 = ThreePointArc(l1 @ 1, (L / 2.0, w / 2.0 + t), (L, w / 2.0))
+l3 = Line(l2 @ 1, ((l2 @ 1).X, 0, 0))
+ln29 = l1 + l2 + l3
+ln29 += mirror(ln29)
+sk29 = make_face(ln29)
+ex29 = extrude(sk29, -(h + b))
+ex29 = fillet(ex29.edges(), radius=w / 6)
 
+neck = Plane(ex29.faces().sort_by().last) * Circle(t)
+ex29 += extrude(neck, n)
+necktopf = ex29.faces().sort_by().last
+example = offset(ex29, -b, openings=necktopf)
 
-def test_lstop_simple():
-    l1 = build_line(Plane.XZ).append(
-        op_start(X(150)),
-        YY(111 / 2),
-        X(-40),
-        op_chamfer(8),
-        YY(65 / 2),
-        X(-23),
-        YY(84 / 2),
-        op_fillet(8, 2),
-        XX(0),
-        YY(0),
-        op_fillet(10),
-        op_close(),
-    )
-    part = l1.revolvex()
-
-    l2 = build_line(Plane.XZ.move(X(150 - 109))).append(
-        op_start(Y(88)),
-        XX(70 / 2),
-        Y(-30),
-        op_chamfer(8),
-        XX(58 / 2),
-        YY(0),
-        op_close(Axis.Y),
-    )
-    p2 = l2.revolvey()
-    part += p2
-
-    hl = build_line(Plane.XZ).append(op_start(X(150)), X(-109), Y(88), op_fillet(30))
-    hole = hl.sweep(Circle(d=40))
-    part -= hole
-
-    cl = build_line(Plane.XZ).append(
-        op_start((150, 75 / 2), -Axis.X),
-        op_line(angle=44 / 2, until=YY(20)),
-        op_close(Axis.X),
-    )
-    cone = cl.revolvex()
-    part -= cone
-    return part
-
-
-example = outer - inner - hole
-show(test_lstop_simple(), example)
+show( example)
 # build123d.export_stl(example, f"/home/niteria/tmp/p_{util.params()}.stl")
 
 # %%
